5e62dc12d3d86493923d86d8f8ce3135bb8ecf9a
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-09-30 23:30:06 -0700

    Testing the DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "classnames";
 
 
-import "components/Button.scss";
+import "./Button.scss";
 
 export default function Button(props) {
   const  buttonClass = classNames('button', {'button--confirm' : props.confirm}, {'button--Base' : props.Base}, {'button--danger' : props.danger});
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,26 @@
 import React from "react";
+import classNames from "classnames";
+
+import "./DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  const  dayClass = classNames('day-list__item ', {'day-list__item--selected' : props.selected}, {'day-list__item--full' : props.spots===0});
+
+  const formatSpots = (spots) => {
+    let spotStr = (spots === 0) ? 'no' : spots;
+    spotStr += (spots === 1) ? ' spot' : ' spots';
+    spotStr +=  ' remaining';    
+    return spotStr;
+  }
+
   return (
     <li>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <div className={dayClass}>
+        <h2>{props.name}</h2> 
+        {/* <h3>{(props.spots === 0) ? 'no': props.spots} spots remaining</h3> */}
+        <h3>{formatSpots(props.spots)}</h3>
+      </div>
     </li>
   );
 }
