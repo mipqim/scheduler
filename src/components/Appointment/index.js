944646f464eb9bd104ce9d50a5b090392f40335b
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-10-03 04:38:33 -0700

    Our First Appointment Views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import Header from "./Header";
+
 import "components/Appointment/style.scss";
 
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header
+        time = {props.time}
+      />
+    </article>
   );
 }
